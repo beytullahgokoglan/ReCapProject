a0bf46533ec3ce19c90e44ef67ce7970ade7ec22
Beytullah GÖKOĞLAN <beytullahgokoglan@hotmail.com>
2021-02-08 01:58:37 +0300

    Revert "Revert "Add project files.""
    This reverts commit 86ce5a6685ac9220faab19b215dd782dd60a3b1b.

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,19 @@
+﻿using Business.Concrete;
+using DataAccess.Concrete;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager carManager = new CarManager(new InMemoryCarDal());
+            foreach (var car in carManager.GetAll())
+            {
+                
+                Console.WriteLine("Id:{0}\nBrandId:{1}\nColorId:{2}\nModelYear:{3}\nDailyPrice:{4}\nDescription:{5}",car.Id,car.BrandId,car.ColorId,car.ModelYear,car.DailyPrice,car.Description);
+            }
+        }
+    }
+}
